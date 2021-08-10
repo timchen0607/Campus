6c5584986b73f13561d98911e2e35c276b4a8d61
Tim School <a0963573232@gmail.com>
2021-08-10 17:09:05 +0800

    Feat: 下拉式選單

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,23 @@
     <header class="header">
       <div class="container">
         <h1 class="header-title">Campus-Web</h1>
-        <h2 class="header-subTitle">班級名稱B</h2>
+        <h2 class="header-subTitle" v-if="userID">
+          <span v-text="groupName"></span>
+          <span class="header-setting" @click="showMenu = !showMenu">
+            &#x2699;
+          </span>
+          <div :class="['header-menu', { show: showMenu }]">
+            <ul>
+              <li v-for="item in groupList" :key="item.key">{{ item.name }}</li>
+            </ul>
+            <hr />
+            <ul>
+              <li>Logs</li>
+              <li>Setting</li>
+              <li>Log Out</li>
+            </ul>
+          </div>
+        </h2>
       </div>
     </header>
     <div class="container">
@@ -29,6 +45,7 @@ export default {
   name: "App",
   data() {
     return {
+      showMenu: false,
       userID: "",
       userName: "",
       groupID: "",
@@ -67,12 +84,41 @@ export default {
     align-items: center;
   }
   &-title {
-    font-size: 2rem;
+    font-size: 2.5rem;
     color: $c_light;
   }
   &-subTitle {
+    position: relative;
     font-size: 1.5rem;
     color: $c_secondary;
+  }
+  &-setting {
+    display: inline-block;
+    margin-left: 1rem;
+    padding: 0.4rem;
+    cursor: pointer;
+    transition: transform 0.5s;
+    &:hover {
+      color: $c_warning;
+      transform: rotate(180deg);
+    }
+  }
+  &-menu {
+    position: absolute;
+    top: calc(100% + 1.2rem);
+    right: 0.5rem;
+    padding: 1rem;
+    color: $c_success;
+    background-color: $c_secondary-light;
+    border: 1px solid $c_secondary;
+    transform: translateY(-130%);
+    transition: transform 0.5s;
+    hr {
+      border: 1px solid $c_secondary;
+    }
+    &.show {
+      transform: translateY(0%);
+    }
   }
 }
 </style>
