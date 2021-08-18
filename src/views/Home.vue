bafafc87bf93d0be1aa743de93aaea73399254a8
Tim School <a0963573232@gmail.com>
2021-08-18 16:22:14 +0800

    Feat: 刪除功能

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,31 @@
 <template>
   <div class="home">
     <div class="list" v-for="item in article" :key="item.key">
-      <router-link :to="'/' + groupID + '/' + item.key">
-        <div>
-          <div v-text="item.reply"></div>
-          <div>
-            <div v-text="item.title"></div>
-            <div>
-              <div v-text="item.authorName"></div>
-              <div v-text="item.timeStamp"></div>
+      <div class="list_item">
+        <router-link class="list_link" :to="'/' + groupID + '/' + item.key">
+          <div
+            :class="{
+              list_reply: true,
+              l1: item.reply < 10,
+              l2: item.reply >= 10,
+              l3: item.reply >= 30,
+              l4: item.reply >= 50,
+            }"
+            v-text="item.reply"
+          ></div>
+          <div class="list_main">
+            <div class="list_title" v-text="item.title"></div>
+            <div class="list_subTitle">
+              <span v-text="item.authorName"></span>
+              <span v-text="item.timeStamp"></span>
             </div>
-            <div>&#128465;</div>
           </div>
+        </router-link>
+        <div class="list_del" @click="delArticle(item.key)" v-if="auth > 1">
+          &#128465;
         </div>
-      </router-link>
+      </div>
     </div>
-    <hr />
-    {{ article[0] }}
-    <hr />
-    {{ logs }}
-    <hr />
-    {{ this.$route.params.groupID }}
-    <hr />
-    {{ this.$route.params.articleID }}
   </div>
 </template>
 
@@ -38,6 +41,7 @@ export default {
       data: null,
       article: [],
       logs: [],
+      auth: 0,
     };
   },
   computed: {
@@ -65,9 +69,9 @@ export default {
   },
   methods: {
     loadData(result) {
-      if (!result) return;
       this.article.length = 0;
       this.logs.length = 0;
+      if (!result) return;
       this.data = JSON.parse(JSON.stringify(result));
       Object.keys(result).forEach((key) => {
         let temp = result[key];
@@ -79,7 +83,104 @@ export default {
       });
       this.article = sortDT(this.article);
       this.setGroupInfo(this.groupID);
+      firebase
+        .database()
+        .ref("/member/" + this.userID + "/auth/" + this.groupID)
+        .on("value", (res) => (this.auth = res.val()));
+    },
+    delArticle(key) {
+      const delFlag = confirm("確定要刪除?刪除後無法恢復此資料。");
+      if (delFlag) {
+        firebase
+          .database()
+          .ref("/article/" + this.groupID + "/" + key)
+          .remove();
+        // this.pushLogs("delete");
+      }
     },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/_variables";
+
+.list {
+  max-width: 1000px;
+  margin: 0 auto;
+  &_item {
+    display: flex;
+    align-items: center;
+    margin: min(2vw, 1rem) 0;
+    border-radius: 0.5rem;
+    transition: background 0.5s;
+    &:hover {
+      background-color: $c_secondary;
+    }
+  }
+  &_link {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    padding: min(2vw, 1rem);
+    text-decoration: none;
+  }
+  &_reply {
+    width: min(15vw, 100px);
+    margin-right: min(2vw, 1rem);
+    padding: min(3vw, 1rem) 0;
+    color: $c_light;
+    font-size: min(10vw, 3rem);
+    text-align: center;
+    border-radius: 5px;
+    &.l1 {
+      background-color: $c_warning;
+    }
+    &.l2 {
+      background-color: #f75000;
+    }
+    &.l3 {
+      background-color: #bf0060;
+    }
+    &.l4 {
+      background-color: $c_dark;
+    }
+  }
+  &_main {
+    width: 0;
+    flex: 1;
+  }
+  &_title {
+    padding: min(1.5vw, 0.5rem);
+    color: $c_dark;
+    font-size: min(5vw, 2rem);
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    background-color: $c_success;
+    overflow: hidden;
+  }
+  &_subTitle {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: min(1.5vw, 0.5rem);
+    color: $c_dark;
+    font-size: min(3vw, 1rem);
+    background-color: gainsboro;
+  }
+  &_del {
+    margin-right: min(2vw, 1rem);
+    padding: min(2vw, 1rem);
+    font-size: min(10vw, 3rem);
+    color: $c_danger;
+    border: 1px solid $c_danger;
+    border-radius: 5px;
+    transition: 0.5s;
+    cursor: pointer;
+    &:hover {
+      color: $c_light;
+      background-color: $c_danger;
+    }
+  }
+}
+</style>
